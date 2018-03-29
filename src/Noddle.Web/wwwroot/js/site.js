2a47f18dcae82e77e5d2c9cc7f577d6dc069076f
Claire Furney <infuerno@fastmail.fm>
2018-03-30 00:37:11 +0100

    Basic view rotation

--- src/Noddle.Web/wwwroot/js/site.js
+++ src/Noddle.Web/wwwroot/js/site.js
@@ -1,1 +1,7 @@
 ﻿// Write your JavaScript code.
+$(function () {
+    var duration = 60000; // 5000ms = 5s
+    setInterval(function(){ 
+        //window.location.assign("/Rotator/Next")
+    }, duration);
+});
