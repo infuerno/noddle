d27b362a5881b26639a9ae7788cbe48965f71880
Claire Furney <infuerno@fastmail.fm>
2018-03-31 21:27:56 +0100

    Add clock

--- src/Noddle.Web/wwwroot/js/site.js
+++ src/Noddle.Web/wwwroot/js/site.js
@@ -1,6 +1,8 @@
 ﻿// Write your JavaScript code.
 $(function () {
-    var rotateDuration = 5000; // 5000ms = 5s
+	$('li.nav-item > a[href="' + window.location.pathname + '"]').parent().addClass('active');
+
+    var rotateDuration = 20000; // 5000ms = 5s
     var refreshCountdownDuration = 1000; // 1s
     var timeUntilNextRotation = rotateDuration;
     $("#rotator-countdown").text(timeUntilNextRotation / 1000);
@@ -14,5 +16,20 @@ $(function () {
     setInterval(function () {
     	timeUntilNextRotation -= refreshCountdownDuration;
     	$("#rotator-countdown").text(timeUntilNextRotation / 1000);
-    }, refreshCountdownDuration);    
+    }, refreshCountdownDuration);
+
+	var clock = new StationClock("clock");
+	clock.body = StationClock.NoBody;
+	clock.dial = StationClock.GermanStrokeDial;
+	clock.hourHand = StationClock.PointedHourHand;
+	clock.minuteHand = StationClock.PointedMinuteHand;
+	clock.secondHand = StationClock.HoleShapedSecondHand;
+	clock.boss = StationClock.NoBoss;
+	clock.minuteHandBehavoir = StationClock.BouncingMinuteHand;
+	clock.secondHandBehavoir = StationClock.OverhastySecondHand;
+
+    clock.dialColor = 'rgb(360,360,360)';
+
+	window.setInterval(function() { clock.draw() }, 50);
 });
+
